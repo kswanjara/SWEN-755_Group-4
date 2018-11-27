679079b373a322eba534c3a9a21333d31ea1823d
TheEibwen <ehteibwen@gmail.com>
2018-11-26 19:43:00 -0500

    Init

---
+++ Assignment4/controllers/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+const auth = function(req, res, next) {
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+};
+const notAuth = function(req, res, next) {
+    if (!(req.session && req.session.user)) {
+        return next();
+    } else {
+        res.redirect('/');
+    }
+};
+
+router.get('/', auth, (req, res) => {
+    res.render('index', {
+        username: req.session.user
+    });
+});
+
+router.get('/register', notAuth, (req, res) => {
+    res.render('register', {
+        complete: false,
+        error: false
+    });
+});
+
+router.get('/login', notAuth, (req, res) => {
+    res.render('login', {
+        complete: false,
+        error: false
+    });
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+});
+
+router.post('/register', notAuth, function (req, res) {
+    User.forge({
+            username: req.body.username,
+            password: bcrypt.hashSync(req.body.password, 10)
+        })
+        .save()
+        .then(function (user) {
+            res.render('register', {
+                complete: true,
+                error: false
+            });
+        })
+        .catch(function (err) {
+            res.render('register', {
+                complete: false,
+                error: true
+            });
+        });
+});
+
+router.post('/login', notAuth, function (req, res) {
+    User.where('username', req.body.username)
+        .fetch()
+        .then(function (user) {
+            if (user) {
+                bcrypt.compare(req.body.password, user.get('password'), function(err, match) {
+                    if (match) {
+                        // saves the user's info to the session
+                        req.session.user = user.get('username');
+                        req.session.role = user.get('role');
+
+                        res.render('login', {
+                            complete: true,
+                            error: false
+                        });
+                    } else {
+                        res.render('login', {
+                            complete: false,
+                            error: true
+                        });
+                    }
+                });
+            } else {
+                res.render('login', {
+                    complete: false,
+                    error: true
+                });
+            }
+        })
+        .catch(function (err) {
+            res.render('login', {
+                complete: false,
+                error: true
+            });
+        });
+});
+
+module.exports = router;
